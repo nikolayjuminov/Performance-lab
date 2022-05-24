7df846beac66f2ff5d35e3673a325565ca506e9a
nikolayjuminov <nikolayjuminov@yandex.ru>
2022-05-25 01:26:06 +0400

    task1 resolved

--- task1/src/task1.java
+++ task1/src/task1.java
@@ -1,5 +1,23 @@
 public class task1 {
+    public task1() {
+    }
+
     public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int m = Integer.parseInt(args[1]) - 1;
+        int[] array = new int[n];
+
+        int idx;
+        for(idx = 0; idx < n; ++idx) {
+            array[idx] = idx + 1;
+        }
+
+        idx = 0;
+
+        do {
+            System.out.print(array[idx]);
+            idx = (idx + m) % n;
+        } while(idx != 0);
 
     }
 }
